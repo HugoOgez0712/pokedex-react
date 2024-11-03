1bfca7d5b5a0368cb7bb62b68b77aac62b23b507
HugoOgez0712 <zego.hugo@gmail.com>
2024-11-03 11:02:05 +0100

    searchbar added

--- Pokedex-react/src/App.tsx
+++ Pokedex-react/src/App.tsx
@@ -25,6 +25,7 @@ interface Pokemon {
 function App() {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
   const [loading, setLoading] = useState(true);
+  const [searchTerms, setSearchTerms] = useState('');
 
   const fetchPokemons = async () => {
     try {
@@ -45,35 +46,58 @@ function App() {
     }
     
   }
-
+  const filteredPokemon = () => {
+    return pokemons.filter(pokemon =>
+      pokemon.name.toLowerCase().includes(searchTerms.toLowerCase())
+    );
+  };
   useEffect(() => {
     fetchPokemons();
   }, []);
 
+
+
   return (
-  <div className="App">
-    <div className="title-section mb-4">
-    <div className="container">
-      <div className="row">
-        <div className="col-12 text-center">
+<div className="App">
+  <div className="title-section mb-4">
+        <div className="container">
+
+          <div className="row">
+            <div className="col-12 text-center">
           <h1 className="main-title">Pokédex</h1>
           <p className="subtitle">Search for Pokémon by name or using the National Pokédex number</p>
         </div>
+        <div className="container mb-4">
+        
+
+  <div className="container">
+  <div className="row justify-content-center">
+    {/* Search bar will be:
+        - Full width on mobile (<768px)
+        - Half width on medium and up (≥768px) */}
+    <div className="col-12 col-md-6">
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Search Pokémon..."
+        value={searchTerms}
+        onChange={(e) => setSearchTerms(e.target.value)}
+      />
+          </div>
+        </div>
+      </div>
+
+
       </div>
     </div>
     {loading ? (
       <p className="text-center">Loading...</p>
     ) : (
       <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
-        {pokemons.map((pokemon) => (
+        {filteredPokemon().map((pokemon) => (
           <div className="col" key={pokemon.id}>
             <div className="card h-100 shadow-sm hover-card">
-              <img 
-                src={pokemon.sprites.front_default} 
-                alt={pokemon.name}
-                className="card-img-top p-3"
-              />
-              <div className="card-body">
+            <div className="card-body">
                 <h5 className="card-title text-center text-capitalize mb-2">
                   {pokemon.name}
                 </h5>
@@ -88,11 +112,18 @@ function App() {
                   ))}
                 </div>
               </div>
+              <img 
+                src={pokemon.sprites.front_default} 
+                alt={pokemon.name}
+                className="card-img-bottom p-1"
+              />
+             
             </div>
           </div>
         ))}
       </div>
     )}
+  </div>
   </div>
 </div>
   )
